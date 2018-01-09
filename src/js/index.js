ac5cf016ab1273adf28a0f8915a43f1510dfe8e8
Lucas Caires <lucas@Mac-mini-de-Lucas.local>
2018-01-09 12:02:43 -0200

    Inserting jquery and bootstrap plugin

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,22 @@
 /* Main JS File */ 
 
-/* If you want bootstrap, do not remove the above line */
-let bootstrap = require('../sass/main.scss')
+/**
+ * Load de main SASS file
+ * You can also customize the bootstrap sass import there
+ */
+require('../sass/main.scss')
 
+/**
+ * Load jQuery and Bootstrap plugin
+ * This is optinal, so you can remove or comment the code below.
+ */
+try {
+	window.$ = window.jQuery = require('jquery')
+	require('bootstrap/dist/js/bootstrap.bundle')
+} catch (e) {}
+
+/**
+ * Now you'll be ready to develop your application
+ * using bootstrap core files.
+ */
 console.log('Your app is running...')
